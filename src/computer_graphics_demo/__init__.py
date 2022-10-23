63b20e694da782225b3d9e6f48f14bfdcd615a59
Wu Zhenyu <wuzy01@qq.com>
2022-10-24 00:08:23 +0800

    :art: Remove setup.py

--- src/computer_graphics_demo/__init__.py
+++ src/computer_graphics_demo/__init__.py
@@ -1,18 +1,7 @@
 """``computer_graphics_demo/__init__.py``."""
 
-import logging
 from typing import Final
 
-logger = logging.getLogger(__name__)
-
-try:
-    from get_version import get_version, NoVersionFound
-
-    try:
-        __version__ = get_version(__file__)
-    except NoVersionFound:
-        __version__ = "0.0.0"
-except ImportError:
-    __version__ = "Please install get_version!"
+from ._version import __version__
 
 _binname: Final = "cgdemo"
